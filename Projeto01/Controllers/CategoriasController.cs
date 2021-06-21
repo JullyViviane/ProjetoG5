54b296f0a849e06222df9758463b48cba4f83ded
JullyViviane <47670952+JullyViviane@users.noreply.github.com>
2021-06-21 18:35:54 -0300

    Update CategoriasController.cs

--- Projeto01/Controllers/CategoriasController.cs
+++ Projeto01/Controllers/CategoriasController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -23,6 +23,61 @@ namespace Projeto01.Controllers
         public ActionResult Index()
         {
             return View(categorias);
+        }
+
+        // GET: Categorias
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+        // POST: Categorias Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            categorias.Add(categoria);
+            categoria.CategoriaID = categorias.Select(m => m.CategoriaID).Max() + 1;
+            return RedirectToAction("Index");
+        }
+
+        // GET: Categorias
+        public ActionResult Edit(long id)
+        {
+            return View(categorias.Where(m => m.CategoriaID == id).First());
+        }
+
+        // POST: Categorias Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categoria categoria)
+        {
+            categorias.Remove(
+            categorias.Where(c => c.CategoriaID == categoria.CategoriaID).First());
+            categorias.Add(categoria);
+            return RedirectToAction("Index");
+        }
+
+        // GET: Categorias Details
+        public ActionResult Details(long id)
+        {
+            return View(categorias.Where(m => m.CategoriaID == id).First());
+        }
+
+        // GET: Categorias Delete
+        public ActionResult Delete(long id)
+        {
+            return View(categorias.Where(m => m.CategoriaID == id).First());
+        }
+
+        // POST: Categorias Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Categoria categoria)
+        {
+            categorias.Remove(
+            categorias.Where(c => c.CategoriaID == categoria.CategoriaID).First());
+            return RedirectToAction("Index");
         }
     }
 }
