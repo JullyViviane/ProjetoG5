16a19b41c4754247bc4d91ef986d243990cdbf5c
JullyViviane <47670952+JullyViviane@users.noreply.github.com>
2021-07-06 14:28:04 -0300

    Add files via upload

---
+++ Projeto01/Context/EFContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Projeto01.Models;
+
+namespace Projeto01.Context
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("Asp_Net_MVC5_CS") { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+    }
+}
