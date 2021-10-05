fb9f5199dd667746605c4be54aa51a1325f227d6
JullyViviane <47670952+JullyViviane@users.noreply.github.com>
2021-10-05 14:21:58 -0300

    Add files via upload

---
+++ PersistênciaDALCadastros ProdutoDAL.cs
@@ -0,0 +1,49 @@
+usando o  Modelo . Cadastros ;
+usando o  Modelo . Tabelas ;
+usando  Persistencia . Contextos ;
+using  System ;
+usando o  sistema . Coleções . Genérico ;
+usando o  sistema . Dados . Entidade ;
+usando o  sistema . Linq ;
+usando o  sistema . Texto ;
+usando o  sistema . Threading . Tarefas ;
+
+namespace  Persistir ê ncia . DAL . Cadastros
+{
+    public  class  ProdutoDAL
+    {
+         contexto EFContext  privado = novo EFContext ();   
+
+        public  IQueryable < Produto > ObterProdutosClassificadosPorNome ()
+        {
+             contexto de retorno . Produtos . Incluir ( c  =>  c . Categoria ). Inclui ( f  =>  f . Fabricante ).
+            OrderBy ( n  =>  n . Nome );
+        }
+
+        public  Produto  ObterProdutoPorId ( long  id )
+        {
+             contexto de retorno . Produtos . Onde ( p  =>  p . ProdutoId  ==  id ). Incluir ( c  =>  c . Categoria ). Inclui ( f  =>  f . Fabricante ). Primeiro ();
+        }
+
+        public  void  GravarProduto ( Produto  produto )
+        {
+            if ( produto . ProdutoId  ==  null )
+            {
+                contexto . Produtos . Adicionar ( produto );
+            }
+            outro
+            {
+                contexto . Entrada ( produto ). State  =  EntityState . Modificado ;
+            }
+            contexto . SaveChanges ();
+        }
+
+        public  Produto  EliminarProdutoPorId ( id longo  )
+        {
+            Produto  produto  =  ObterProdutoPorId ( id );
+            contexto . Produtos . Remover ( produto );
+            contexto . SaveChanges ();
+            return  produto ;
+        }
+    }
+}
